c16f3e99019afbb985a57e147df8dda1bbb1158e
Melissa <melissakalish2@gmail.com>
2023-08-15 22:35:48 -0500

    added javascript code for api information

--- public/js/script.js
+++ public/js/script.js
@@ -1,15 +1,58 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const apiDataContainer = document.getElementById('api-data');
-  
-    fetch('/api/plants') // Replace 'plants' with the desired API endpoint
-      .then(response => response.json())
-      .then(data => {
-        // Display the fetched data on the website
-        apiDataContainer.textContent = JSON.stringify(data, null, 2);
-      })
-      .catch(error => {
-        console.error('Error fetching API data:', error);
-        apiDataContainer.textContent = 'An error occurred while fetching API data.';
-      });
-  });
-  
+  const searchBar = document.querySelector(".search-bar");
+  const searchButton = document.getElementById("search-button");
+  const searchInput = document.getElementById("search-input");
+  const form = document.getElementById("form");
+  const apiKey = "sk-QuSe64daf50463a5f1880";
+
+function fetchDataFromAPI() {
+  const apiUrl = `GET https://perenual.com/api/species-list?q=${common_name}page=1&key=${apiKey}`;
+
+  fetch(apiUrl)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(`Network response was not ok, Status: ${response.status}`);
+      }
+      return response.json();
+    })
+    .then(data => {
+      // Process the data
+      const speciesData = data.data;
+      for (const species of speciesData) {
+        console.log("Common Name:", species.common_name);
+        console.log("Scientific Name:", species.scientific_name[0]);
+        console.log("Other Names:", species.other_name.join(", "));
+        console.log("Cycle:", species.cycle);
+        console.log("Watering:", species.watering);
+        console.log("Sunlight:", species.sunlight.join(", "));
+        console.log("Image URL:", species.default_image.original_url);
+        console.log("===========================");
+      }
+    })
+    .catch(error => {
+      console.error("An error occurred:", error);
+    });
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  const productContainer = document.querySelector('.product-list');
+
+  fetch(apiUrl) // Replace with the actual API route
+    .then(response => response.json())
+    .then(products => {
+      const source = document.querySelector('#product-template').innerHTML;
+      const template = Handlebars.compile(source);
+      const context = { products };
+      const html = template(context);
+      productContainer.innerHTML = html;
+    })
+    .catch(error => {
+      console.error('Error fetching product data:', error);
+    });
+});
+
+
+// Call the function to fetch and process data
+fetchDataFromAPI();
+
+})
