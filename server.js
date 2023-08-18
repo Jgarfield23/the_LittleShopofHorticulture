3f7116ed7a3bd04eb59abb408fefd37e928d4369
Ji-yoh <geovannih514@gmail.com>
2023-08-17 20:02:27 -0700

    updated PORT variable name on server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const session = require('express-session')
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const sequelize = require('./connection/connection');
 
-const port = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
 const expHbs = expressHandlebars.create({
   helpers,
