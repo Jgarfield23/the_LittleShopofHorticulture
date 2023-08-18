fefdf74129978c35c39b14629f73e56791aa1bd3
Jgarfias23 <garfroj.j97@gmail.com>
2023-08-18 11:15:05 -0700

    small fixes with Elena

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const app = express();
 const fetch = require('node-fetch');
 const expressHandlebars = require('express-handlebars');
 const helpers = require("./utils/helpers");
@@ -7,8 +6,9 @@ const routes = require('./routes');
 // merged for user routes and session
 const path = require('path');
 const session = require('express-session')
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const sequelize = require('./connection/connection');
+const app = express();
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const PORT = process.env.PORT || 3000;
 
@@ -24,23 +24,23 @@ app.set('view engine', 'handlebars');
 app.use(express.static('public'));
 app.use(express.json());
 app.use(routes);
-// app.use(session(userSession))
 
-/* const userSession = {
-    secret: process.env.SESSION_SECRET,
-    cookie: {
-        maxAge: 60 * 60 * 2000,
-        httpOnly: true,
-        secure: false,
-        sameSite: 'strict',
-    },
-    resave: false,
-    saveUninitialized: true,
-    store: new SequelizeStore({
-        db: sequelize,
-    })
-}; */
+const userSession = {
+  secret: process.env.SESSION_SECRET,
+  cookie: {
+    maxAge: 60 * 60 * 2000,
+    httpOnly: true,
+    secure: false,
+    sameSite: 'strict',
+  },
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  })
+}; 
 
+app.use(session(userSession))
 app.get('/', (req, res) => {
     res.render("login");
 });
@@ -50,7 +50,7 @@ app.get('/products', (req, res) => {
   res.render('products', {products: fetchedProducts});
 });
 
-sequelize.sync({ force: true }).then(() => {
+sequelize.sync({ force: false }).then(() => {
     app.listen(PORT, () => {
         console.log(`App listening on port ${PORT}`)
     })
