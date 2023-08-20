5242bc13ebf748c16088562be28b428ce7bcdc36
Melissa <melissakalish2@gmail.com>
2023-08-20 12:34:32 -0500

    bug fixes

--- server.js
+++ server.js
@@ -10,7 +10,6 @@ const session = require('express-session')
 const sequelize = require('./connection/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
-
 const PORT = process.env.PORT || 3000;
 
 const expHbs = expressHandlebars.create({
@@ -25,9 +24,8 @@ app.set('view engine', 'handlebars');
 app.use(express.static('public'));
 app.use(express.json());
 app.use(routes);
-// app.use(session(userSession))
 
- const userSession = {
+const userSession = {
     secret: process.env.SESSION_SECRET,
     cookie: {
         maxAge: 60 * 60 * 2000,
@@ -43,20 +41,23 @@ app.use(routes);
 }; 
 
 app.use(session(userSession))
+
 app.get('/', (req, res) => {
     res.render("login");
 });
 
 app.get('/products', (req, res) => {
-  //fetch products from API and pass them to the view
+  // Fetch products from API and pass them to the view
   res.render('products', {products: fetchedProducts});
 });
 
+// Move Sequelize sync here
 sequelize.sync({ force: true }).then(() => {
     app.listen(PORT, () => {
         console.log(`App listening on port ${PORT}`)
-    })
+    });
 });
+
 
 
 
