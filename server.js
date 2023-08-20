f6bcfdf7625f325bb88e6126dd58026f36901e28
Melissa <melissakalish2@gmail.com>
2023-08-20 09:16:25 -0500

    changes with Elena

--- server.js
+++ server.js
@@ -7,8 +7,10 @@ const routes = require('./routes');
 // merged for user routes and session
 const path = require('path');
 const session = require('express-session')
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const sequelize = require('./connection/connection');
+const app = express();
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
 
 const PORT = process.env.PORT || 3000;
 
@@ -26,7 +28,7 @@ app.use(express.json());
 app.use(routes);
 // app.use(session(userSession))
 
-/* const userSession = {
+ const userSession = {
     secret: process.env.SESSION_SECRET,
     cookie: {
         maxAge: 60 * 60 * 2000,
@@ -39,8 +41,9 @@ app.use(routes);
     store: new SequelizeStore({
         db: sequelize,
     })
-}; */
+}; 
 
+app.use(session(userSession))
 app.get('/', (req, res) => {
     res.render("login");
 });
